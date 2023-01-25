d2158a5afcc3e35bc093925dee062ab5406c5bf3
BohdanMedinsky <mbogdan4ik2001@gmail.com>
2023-01-25 17:57:10 +0200

    first commit

--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
-  styleUrls: ['./second.component.css']
+  styleUrls: ['./second.component.css'],
+  
 })
 export class SecondComponent {
-
+ name:string ='Мединський Богдан Петрович' 
+ email:string ='mbogdanp2001@gmail.com'
+salary: number = 2000
+course: number = 0.50
+age: number = 21
 }
