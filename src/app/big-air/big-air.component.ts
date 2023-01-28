66d10c2a61fdb3e7d1e24f605b0637770c201195
BohdanMedinsky <mbogdan4ik2001@gmail.com>
2023-01-28 16:47:05 +0200

    first commit

--- src/app/big-air/big-air.component.ts
+++ src/app/big-air/big-air.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-air.component.css']
 })
 export class BigAirComponent {
-
+  Name:string ='Найбільший літак' 
+  hist:string ='Ан-225 «Мрія» (кодове імя НАТО — «Козак» англ. Cossack) — найбільший та найпотужніший у світі транспортний літак , створений київським КБ імені Антонова. Головний конструктор — Віктор Ілліч Толмачов.' 
+  price:number = 300000
+  pt:string ='складає 6100 к.с' 
+  date:string = '21 грудня 1988'
 }
--- src/app/big-ship/big-ship.component.ts
+++ src/app/big-ship/big-ship.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-ship.component.css']
 })
 export class BigShipComponent {
-
+  Name:string ='Найбільший  лайнер' 
+  hist:string ='Harmony of the Seas (укр. Гармонія морів) — найбільший круїзний лайнер у світі. Вага судна — 120 тисяч тонн, висота — 70 метрів, ширина — 66 метрів, загальна довжина — 362 метри.' 
+  price:number = 1400000000
+  pt:string ='складає 6100 к.с' 
+  date:string = '21 листопада 2008'
 }
