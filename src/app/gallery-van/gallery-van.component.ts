722cedc11280b53d92b8f001296f025be42f7a7c
BohdanMedinsky <mbogdan4ik2001@gmail.com>
2023-01-31 17:41:23 +0200

    gallery

--- src/app/gallery-van/gallery-van.component.ts
+++ src/app/gallery-van/gallery-van.component.ts
@@ -9,13 +9,16 @@ export class GalleryVanComponent {
 
   Name:string ='Автопортрет' 
   avtor:string ='Ван-Гог ' 
- maps:string ='Чикаго,США' 
+ maps:string ='Чикаго,США'
+ price:number = 53000000  
 
   name:string ='Едоки Картофеля' 
   Avtor:string ='Ван-Гог ' 
-  Maps:string ='Музей Ван-Гога' 
+  Maps:string ='Музей Ван-Гога'
+  pricec:number = 60000000  
 
   Names:string ='Іриси' 
   avtors:string ='Ван-Гог ' 
  mapss:string ='J.Paul Getty Muzeum'
+ prices:number = 700000000 
 }
